bd8ec0a10814f7a0deaadb78a86919d73dede28c
Chan Nyein Tun <29679269+channyeintun@users.noreply.github.com>
2025-12-16 06:39:13 +0700

    chore: fix ignore build script

--- scripts/ignore-build.sh
+++ scripts/ignore-build.sh
@@ -2,10 +2,10 @@
 
 echo "Deployment source: $VERCEL_DEPLOYMENT_SOURCE"
 
-if [ "$VERCEL_DEPLOYMENT_SOURCE" = "deploy-hook" ]; then
-  echo "Allowing build (deploy hook)"
+if [ "$VERCEL_DEPLOYMENT_SOURCE" = "github" ]; then
+  echo "Allowing build (github deploy hook)"
   exit 1   # ❗ continue build
 fi
 
-echo "Ignoring build (not deploy hook)"
+echo "Ignoring build (not github deploy hook)"
 exit 0     # ❗ skip build
