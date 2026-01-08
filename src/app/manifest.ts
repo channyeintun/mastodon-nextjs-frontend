a8796e098296fd3c90a9b1247d260a011282497a
Chan Nyein Tun <29679269+channyeintun@users.noreply.github.com>
2026-01-08 23:43:00 +0700

    fix: fix icons in manifest

--- src/app/manifest.ts
+++ src/app/manifest.ts
@@ -11,13 +11,18 @@ export default function manifest(): MetadataRoute.Manifest {
         theme_color: '#6364ff',
         icons: [
             {
-                src: '/icon.png',
-                sizes: '32x32',
+                src: '/icons/icon-192.png',
+                sizes: '192x192',
                 type: 'image/png',
             },
             {
-                src: '/apple-icon.png',
-                sizes: '180x180',
+                src: '/icons/icon-512.png',
+                sizes: '512x512',
+                type: 'image/png',
+            },
+            {
+                src: '/icons/icon-512.png',
+                sizes: '512x512',
                 type: 'image/png',
                 purpose: 'maskable',
             },
